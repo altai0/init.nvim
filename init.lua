2884e7b2320d60c2bf58a1f5a9aa4d072499937f
Oğuzhan <oguzbeyaz06@gmail.com>
2022-11-20 18:57:00 +0300

    init

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Altai neovim setttings
+
+
+require("configs.options") 
+require("configs.plugins")
+require("configs.highlighting")
+require("configs.completion")
+require("configs.diagnostics")
+require("configs.languages")
+require("configs.formatting")
+require("configs.keymaps")
+require("configs.theming")
